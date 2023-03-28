bb3fbbb4f6f4394b0f61e18adeeb70e66da4a536
Azurel2022 <azurel.kv@gmail.com>
2023-03-28 21:59:36 +0200

    SAVE SPOT

--- budget-ui-template/src/app/category/category-modal/category-modal.component.ts
+++ budget-ui-template/src/app/category/category-modal/category-modal.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { ActionSheetService } from '../../shared/service/action-sheet.service';
-import { filter, from } from 'rxjs';
+import {Component} from '@angular/core';
+import {ModalController} from '@ionic/angular';
+import {ActionSheetService} from '../../shared/service/action-sheet.service';
+import {filter, from} from 'rxjs';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Category} from "../../shared/domain";
+import {ToastService} from "../../shared/service/toast.service";
+import {save} from "ionicons/icons";
+
+class CategoryService {
+}
 
 @Component({
   selector: 'app-category-modal',
   templateUrl: './category-modal.component.html',
 })
 export class CategoryModalComponent {
+  // Passed into the component by the ModalController, available in the ngOnInit
   category: Category = {} as Category;
 
   readonly categoryForm: FormGroup;
   submitting = false;
-  constructor(private readonly actionSheetService: ActionSheetService, private readonly modalCtrl: ModalController) {}
+
+  constructor(
+    private readonly actionSheetService: ActionSheetService,
+    private readonly categoryService: CategoryService,
+    private readonly formBuilder: FormBuilder,
+    private readonly modalCtrl: ModalController,
+    private readonly toastService: ToastService
+  ) {
+    this.categoryForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(40)]],
+    });
+  }
 
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
